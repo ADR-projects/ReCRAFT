890ce4be4410fdb79c54646663be159cc069e757
ADR-projects <i.adrijadas@gmail.com>
2025-11-09 02:31:55 +0530

    Remove unnecessary imports

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
 'use client';
 import Header from '../components/Header.jsx'
-import { useState } from 'react';
-import Link from 'next/link';
-import { Plus, X, Edit2 } from 'lucide-react';
-import { crafts, logos } from '../assets/crafts.js'
 import ProfileSection from '@/components/ProfileSection.jsx';
 import Materials from '@/components/Materials.jsx';
 import CraftsGrid from '@/components/CraftsGrid.jsx';
